e919101d0a6a053f648a3d30f250be3aa956202e
Nathan <natemi1010@gmail.com>
2019-12-04 17:00:21 +0000

    End Of Video 5 - Poll Command

--- Discord Bot Tutorial/Commands/FunCommands.cs
+++ Discord Bot Tutorial/Commands/FunCommands.cs
@@ -1,6 +1,9 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 using DSharpPlus.Interactivity;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DiscordBotTutorial.Commands
@@ -44,6 +47,33 @@ namespace DiscordBotTutorial.Commands
             var message = await interactivity.WaitForReactionAsync(x => x.Channel == ctx.Channel).ConfigureAwait(false);
 
             await ctx.Channel.SendMessageAsync(message.Result.Emoji);
+        }
+
+        [Command("poll")]
+        public async Task Poll(CommandContext ctx, TimeSpan duration, params DiscordEmoji[] emojiOptions)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+            var options = emojiOptions.Select(x => x.ToString());
+
+            var pollEmbed = new DiscordEmbedBuilder
+            {
+                Title = "Poll",
+                Description = string.Join(" ", options)
+            };
+
+            var pollMessage = await ctx.Channel.SendMessageAsync(embed: pollEmbed).ConfigureAwait(false);
+
+            foreach(var option in emojiOptions)
+            {
+                await pollMessage.CreateReactionAsync(option).ConfigureAwait(false);
+            }
+
+            var result = await interactivity.CollectReactionsAsync(pollMessage, duration).ConfigureAwait(false);
+            var distinctResult = result.Distinct();
+
+            var results = distinctResult.Select(x => $"{x.Emoji}: {x.Total}");
+
+            await ctx.Channel.SendMessageAsync(string.Join("\n", results)).ConfigureAwait(false);
         }
     }
 }
